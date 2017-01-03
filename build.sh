e89132ea2ffdefb2ea778d7854712558f822a100
gomma <dshwang@raonix.com>
2017-01-03 15:54:44 +0900

    untrac files

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 OPENWRT_HOME=/Volumes/OpenWrt/openwrt
 
-export PATH=$OPENWRT_HOME/staging_dir/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_musl-1.1.11/bin:$PATH
-export STAGING_DIR=/home/gomma/projects/openwrt/staging_dir
+export PATH=$OPENWRT_HOME/staging_dir/toolchain-mipsel_24kec+dsp_gcc-5.3.0_glibc-2.22/bin:$PATH
+export STAGING_DIR=/Volumes/OpenWrt/openwrt/staging_dir
 export CROSS_COMPILE=mipsel-openwrt-linux-
 export PREFIX=/Volumes/OpenWrt/NemoAhn-Gateway/external_libs/openzwave/install
 
